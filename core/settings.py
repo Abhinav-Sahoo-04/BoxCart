442c35dd962f060020d50ab0a3dc7cf796f844dd
Abhinav-Sahoo-04 <sahooabhinav5@gmail.com>
2026-05-13 00:09:55 +0530

    static files settings changed

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,11 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= BASE_DIR / 'static'
-STATICFILES_DIRS=[
-    'core/static'
-] 
+STATIC_URL = '/static/'
+
+# Where collected static files will live
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Where your source static files are
+STATICFILES_DIRS = [
+    BASE_DIR / 'core/static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
